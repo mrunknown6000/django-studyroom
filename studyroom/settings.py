4fc5d8f579dd81b63d3a004e1a8da99120181548
mrunknown6000 <dell06082009@gmail.com>
2022-10-19 20:43:11 +0700

    Railway Debug Atm.1

--- studyroom/settings.py
+++ studyroom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%abnnz^l#o(tgn8sdptokbg81a**7!9+#sw@w6glv8zl&sg!@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-studyroom-production.up.railway.app']
 
 
 # Application definition
